805f5b2c2fa0f8cd717b4ed3487f6cb7c16a31ee
lmdrew96 <lmdrew@udel.edu>
2026-07-26 16:16:00 -0400

    feat(title): add new-fonts directory fonts to rotary title animation

--- src/lib/title-fonts.ts
+++ src/lib/title-fonts.ts
@@ -13,6 +13,19 @@ const karen = localFont({ src: "../../fonts/Karen.ttf" });
 const lowballNeue = localFont({ src: "../../fonts/LowballNeueExtraLight.ttf" });
 const superMarioDs = localFont({ src: "../../fonts/SuperMarioDs.ttf" });
 const yoshinese = localFont({ src: "../../fonts/Yoshinese.ttf" });
+const aurekBeshHand = localFont({ src: "../../fonts/new-fonts/AurekBeshHand-pROK.ttf" });
+const britannian = localFont({ src: "../../fonts/new-fonts/Britannian-J4oB.ttf" });
+const etruscan = localFont({ src: "../../fonts/new-fonts/Etruscan-K76A.ttf" });
+const korohanza = localFont({ src: "../../fonts/new-fonts/Korohanza-nRlM.ttf" });
+const luciusCipher = localFont({ src: "../../fonts/new-fonts/LuciusCipher-3zYvy.ttf" });
+const oneVerse = localFont({ src: "../../fonts/new-fonts/OneVerseRegular-PKM4x.ttf" });
+const ptgulShikieika = localFont({ src: "../../fonts/new-fonts/PtgulShikieika-d9Anl.ttf" });
+const reanaarian = localFont({ src: "../../fonts/new-fonts/Reanaarian-j4mG.ttf" });
+const sithAf = localFont({ src: "../../fonts/new-fonts/SithAf-mLlyv.otf" });
+const tau = localFont({ src: "../../fonts/new-fonts/Tau-mmP2.ttf" });
+const tuigan = localFont({ src: "../../fonts/new-fonts/Tuigan-KppX.ttf" });
+const xidusLeadeaNative = localFont({ src: "../../fonts/new-fonts/XidusLeadeaNative-vWAM.ttf" });
+const yugurian = localFont({ src: "../../fonts/new-fonts/Yugurian-KYDZ.ttf" });
 
 export const TITLE_FONTS = [
   blueberryHills,
@@ -26,4 +39,17 @@ export const TITLE_FONTS = [
   lowballNeue,
   superMarioDs,
   yoshinese,
+  aurekBeshHand,
+  britannian,
+  etruscan,
+  korohanza,
+  luciusCipher,
+  oneVerse,
+  ptgulShikieika,
+  reanaarian,
+  sithAf,
+  tau,
+  tuigan,
+  xidusLeadeaNative,
+  yugurian,
 ];
